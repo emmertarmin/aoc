f88f7f8adc07f49d9dcdfa785fc29ca348591141
Armin Emmert <emmert@wirth-horn.de>
2024-12-06 13:10:24 +0100

    update template for 2024

--- 2024/template/index.ts
+++ 2024/template/index.ts
@@ -13,28 +13,31 @@ async function solve(lines: string[]) {
 	return sum
 }
 
-describe(`test`, async () => {
-	const lines = await getLines(`${import.meta.dir}/test1.txt`) as string[]
+describe(`AoC`, async () => {
+	const linesTest = await getLines(`${import.meta.dir}/test1.txt`) as string[]
+	const linesProd = await getLines(`${import.meta.dir}/input.txt`) as string[]
 
-	test('answer 1', async () => {
-		expect(await solve(lines)).toBe(0);
+	describe('PART 1', async () => {
+		test('TEST', async () => {
+			const answer = await solve(linesTest)
+			expect(answer).toBe(0)
+		})
+
+		test('PROD', async () => {
+			const answer = await solve(linesProd)
+			expect(answer).toBe(0)
+		})
 	})
 
-	test('answer 2', async () => {
-		expect(await solve(lines)).toBe(0);
-	})
-})
+	describe('PART 2', async () => {
+		test('TEST', async () => {
+			const answer = await solve(linesTest)
+			expect(answer).toBe(0)
+		})
 
-describe(`prod`, async () => {
-	const lines = await getLines(`${import.meta.dir}/input.txt`) as string[]
-
-	test('answer 1', async () => {
-		const answer = await solve(lines)
-		expect(answer).toBe(0);
-	})
-
-	test('answer 2', async () => {
-		const answer = await solve(lines)
-		expect(answer).toBe(0);
+		test('PROD', async () => {
+			const answer = await solve(linesProd)
+			expect(answer).toBe(0)
+		})
 	})
 })
